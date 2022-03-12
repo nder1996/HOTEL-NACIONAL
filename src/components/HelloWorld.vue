f2b86f5f280b589a96c31e212a4f30d94f01dfeb
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-03-12 01:09:36 -0500

    Primeros cambios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,149 +1,60 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Alpha
-        </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+    <v-container class='container_navbar_1' fluid>
+        <v-row>
+            <v-col class='columnas_navbar' sm='10'>
+                <h5 class='mt-1' style='margin: auto;position: relative;left: 7rem;'>Last COVID19 update - Security measures</h5>
+            </v-col>
+            <v-col class='columnas_navbar d-flex justify-center' sm='2'>
+              <div>
+                 <h6  class='mt-1'>Join us</h6>
+              </div>
+                <div class='icon_navbar d-flex justify-space-between'>
+                    <h6  class='mt-1'>
+                        <v-icon>
+                            mdi-instagram
+                        </v-icon>
+                    </h6>
+                    &nbsp;&nbsp;
+                    <h6  class='mt-1'>
+                        <v-icon>
+                            mdi-facebook
+                        </v-icon>
+                    </h6>
+                    &nbsp;&nbsp;
+                    <h6  class='mt-1'>
+                        <v-icon>
+                            mdi-twitter
+                        </v-icon>
+                    </h6>
+                    &nbsp;&nbsp;
+                </div>
+            </v-col>
         </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-container>
 </template>
-
 <script>
-
 export default {
-  name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+    name: 'HelloWorld',
 }
 </script>
+<style>
+.container_navbar_1 {
+    #border: 5px solid red;
+    background-color: #2C3A52;
+    color: white !important;
+    padding: 15px !important;
+}
+.columnas_navbar {
+    #border: 5px solid black;
+    margin: 0;
+    padding: 0;
+}
+.icon_navbar {
+    position: relative;
+    left: 1rem;
+}
+.icon_navbar p{
+  position: relative;
+  #left: 1rem;
+}
+</style>
