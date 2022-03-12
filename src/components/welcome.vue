c903c57fe6e2b0dea1b2f9ea1b8b93ce849612a6
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-03-12 02:23:26 -0500

    Llegue a galeria
    p

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,20 +1,53 @@
 <template>
-	<v-container>
-		<v-row>
-			
-		</v-row>
-		<v-row>
-			
-		</v-row>
-		<v-row>
-			
-		</v-row>
-
-
-	<v-container/>
+  <div>
+  	<v-container>
+  		<v-row class='img_1'>
+  			<v-col cols='6'>
+  				<div>
+  					<h1>WELCOME TO Hotel Nacional</h1>
+  					<p>
+  						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu magna mauris. Phasellus malesuada turpis nec massa elementum, id consectetur lectus vestibulum. Sed viverra sapien ut elit dapibus, a finibus diam fringilla. In tincidunt molestie odio quis efficitur. Integer fermentum efficitur ligula, a consectetur enim sodales id.
+  					</p>
+  				</div>
+  			</v-col>
+  			<v-col cols='6'>
+  				<v-img class='img' src='@/assets/hotel_1.jpg'>
+  				</v-img>
+  			</v-col>
+  		</v-row>
+  		  		<v-row class='img_1'>
+  			<v-col cols='6'>
+  				<div>
+  					<h1>WELCOME TO Hotel Nacional</h1>
+  					<p>
+  						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu magna mauris. Phasellus malesuada turpis nec massa elementum, id consectetur lectus vestibulum. Sed viverra sapien ut elit dapibus, a finibus diam fringilla. In tincidunt molestie odio quis efficitur. Integer fermentum efficitur ligula, a consectetur enim sodales id.
+  					</p>
+  				</div>
+  			</v-col>
+  			<v-col cols='6'>
+  				<v-img class='img' src='@/assets/hotel_2.jpg'>
+  				</v-img>
+  			</v-col>
+  		</v-row>
+  	</v-container>
+  </div>
 </template>
 <script>
-	export default{
-		name:'welcome'
-	}
+export default {
+    name: 'welcome'
+}
 </script>
+<style scoped>
+	p{
+		text-align: justify;
+	}
+	.img_1{
+		border: 5px solid black;
+		height: 500px;
+	}
+	.img{
+		width: 100%;
+		border: 5px solid purple;
+		height: 300px;
+	}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <navbar_1 />
    <navbar_2 />
    <slider/>
+   <welcome/>
 </template>
 
 <script>
@@ -11,12 +12,13 @@ import { defineComponent } from 'vue';
 import navbar_1 from '../components/Navbar_1.vue';
 import navbar_2 from '../components/Navbar_2.vue';
 import slider from '../components/Slider.vue';
+import welcome from '../components/Welcome.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    navbar_1,navbar_2,slider
+    navbar_1,navbar_2,slider,welcome
   },
 });
 </script>
